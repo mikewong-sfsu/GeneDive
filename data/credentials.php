571520c644c7d31d81f1d960c4c0a99db8bd4e50
Brook Thomas <brookthomas@gmail.com>
2018-01-05 14:22:36 -0800

    replaced gene_id with proper file

---
+++ data/credentials.php
@@ -0,0 +1,9 @@
+<?php
+  
+  // $BASE_URL = "localhost:8888";
+  $BASE_URL = "www.genedive.net";
+  $SMTP_USER = "USER";
+  $SMTP_PASS = "PASS";
+  $SALT = "9a6s8d6a0787&^(*&sdhfajhquihefjksdb";
+
+?>
