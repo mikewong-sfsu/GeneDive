1b1785c90c6c393ee5d32929879968660d4ee4df
jackthesmack <git@jack-cole.com>
2018-03-07 02:51:23 -0800

    added doxygen script to work on UNIX

---
+++ docs/gen/execute_doxygen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Author: Jack Cole jcole2@mail.sfsu.edu
+# Linux uses forward slashes while windows uses backslashes. This can cause problems with directories, which this script aims to fix.
+( cat doxygen.cfg ; echo "OUTPUT_DIRECTORY=../\n"; echo "INPUT = ../../" ) | doxygen
