06a705bf94c7bb81ee2bb14d4343b6a16f4e05f1
John Lazzarini <jlazzari@mail.sfsu.edu>
2018-04-08 20:57:13 -0700

    Returned button to toolbar (for now) so I don't break anything with this build

--- search.php
+++ search.php
@@ -12,8 +12,8 @@
     <div class="module control-module">
             <div class="btn-group" style="width: 100%">
                 <button type="button" class="btn btn-default undo" data-toggle="tooltip-initial" data-placement="bottom" title="Undo the last action you made" disabled> Undo</button>
-                <button type="button" class="btn btn-default reset-graph" data-toggle="tooltip-initial" data-placement="bottom" title="Regenerate the positions of the graph nodes" disabled> Redraw Graph</button>
                 <button type="button" class="btn btn-default redo" data-toggle="tooltip-initial" data-placement="bottom" title="Redo the last action you undid" disabled> Redo</button>
+                <button type="button" class="btn btn-default redo disabled" data-toggle="tooltip-initial" data-placement="bottom" title="Redo the last action you undid"></button>
             </div>
     </div>
 
