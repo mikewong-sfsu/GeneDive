a15c8a7170bcfee7b5d08af72ba7069effa7d170
Jack <jack-cole@users.noreply.github.com>
2018-04-23 00:42:34 -0700

    Heavily updated the testing suite

---
+++ docs/gen/custom_modules/tests/Login.js
@@ -0,0 +1,62 @@
+
+/**
+ @class      ShiftClickGraph
+ @brief      Tests to see if Shift Clicking the graph causes issues
+ @details
+ @authors    Jack Cole jcole2@mail.sfsu.edu
+ @ingroup    tests
+ */
+let Test = require('./Test');
+
+class Login extends Test {
+
+
+  toString() {
+    return "Login"
+  }
+
+  get priority(){
+    return 0;
+  }
+
+  get name(){
+    return "Login";
+  }
+
+  execute() {
+    const thisClass = this;
+    const PAGE = this.page;
+
+    return new Promise(async (resolve, reject) => {
+      try{
+        // If any errors happen on the page, fail the test
+        await thisClass.hookToConsoleErrors(reject);
+
+        console.log("Connecting to " + thisClass.DOMAIN);
+        await PAGE.goto(thisClass.DOMAIN, {waitUntil: 'networkidle2'}).catch((reason)=>{reject(`Unable to connect. ${reason}`)});
+
+        if (thisClass.LOGIN === undefined || this.PASSWORD === undefined)
+          reject("Login or Password not set");
+        await PAGE.click("input#email");
+        await PAGE.keyboard.type(thisClass.LOGIN, {delay:thisClass._TYPING_SPEED});
+        await PAGE.click("input#password");
+        await PAGE.keyboard.type(this.PASSWORD, {delay:thisClass._TYPING_SPEED});
+        await PAGE.click("button");
+        await PAGE.waitForNavigation( {timeout: 5000,waitUntil: 'networkidle2'}).catch(()=>{
+        });
+
+        if(PAGE.url().split("/").pop() !=="search.php"){
+          reject("Was not redirected to search.php");
+        }
+        resolve(thisClass.createResponse(true,"Successfully logged in",0));
+      }
+      catch (e) {
+        reject(e);
+      }
+    })
+
+  }
+}
+
+
+module.exports = Login;
--- docs/gen/screenshots.js
+++ docs/gen/screenshots.js
@@ -3,7 +3,6 @@
  */
 
 
-
 /**
  @brief      Screenshot Generation Script
  @file       screenshots.js
@@ -14,11 +13,40 @@
  [Puppeteer](https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md) is a NoeJS package maintained by Google designed to allow automation of headless Chrome.
  @ingroup tests
  */
+const HEADLESS_MODE = false;
+const ARGUMENTS = process.argv;
 const JSON_FILE = "screenshot_generation.json";
-const action_classes = require('./custom_modules/actions/_import.js');
 const tests = require('./custom_modules/tests/_import.js');
 const puppeteer = require('puppeteer');
-const test_results = {};
+const test_results = [];
+const RESULTS_FILE = `./log/screenshots_results.json`;
+const COLOR = {
+  Reset: "\x1b[0m",
+  Bright: "\x1b[1m",
+  Dim: "\x1b[2m",
+  Underscore: "\x1b[4m",
+  Blink: "\x1b[5m",
+  Reverse: "\x1b[7m",
+  Hidden: "\x1b[8m",
+
+  FgBlack: "\x1b[30m",
+  FgRed: "\x1b[31m",
+  FgGreen: "\x1b[32m",
+  FgYellow: "\x1b[33m",
+  FgBlue: "\x1b[34m",
+  FgMagenta: "\x1b[35m",
+  FgCyan: "\x1b[36m",
+  FgWhite: "\x1b[37m",
+
+  BgBlack: "\x1b[40m",
+  BgRed: "\x1b[41m",
+  BgGreen: "\x1b[42m",
+  BgYellow: "\x1b[43m",
+  BgBlue: "\x1b[44m",
+  BgMagenta: "\x1b[45m",
+  BgCyan: "\x1b[46m",
+  BgWhite: "\x1b[47m",
+};
 
 let json_data;
 
@@ -33,10 +61,6 @@ catch (err) {
   process.exit(1);
 }
 
-// Set the "GLOBAL_DATA", which will apply to all actions
-const DOMAIN = json_data.domain;
-const USERNAME = json_data.login;
-const PASSWORD = json_data.password;
 
 // Make sure the screenshots folder exists
 const SCREENSHOTS_FOLDER = json_data.screenshots_folder;
@@ -47,61 +71,75 @@ if (!fs.existsSync(SCREENSHOTS_FOLDER)) {
 }
 
 // Save any results from a completed test
-const save_test_success = (testName, result, success, test_results)=>{
-  test_results[testName] = {
-    name:testName,
-    status : success ? "pass":"fail",
-    message : result,
-  }
+const save_test_success = (result) => {
+  test_results.push(result);
 };
-// Start Puppeteer browser
+
+
+// Processs a single test
+const do_test = async (test, browser, json_data) => {
+  let promise;
+  let page = await browser.newPage();
+  let singleTest = new test(page, json_data);
+
+  promise = singleTest.execute().then((reason) => {
+    console.log(`${singleTest.toString()}: ${COLOR.FgGreen}PASS${COLOR.Reset}`);
+    save_test_success(reason);
+    page.close();
+  })
+    .catch((reason) => {
+      page.screenshot({path: `${singleTest.name}-error.png`});
+      console.log(`${singleTest.toString()}: ${COLOR.FgRed}FAIL${COLOR.Reset}`);
+      save_test_success(reason);
+      page.close();
+    });
+  return promise;
+};
+
+// Process arguments
+let arguments_trimmed = [];
+for (let i = 0; i < ARGUMENTS.length; i++)
+  if (ARGUMENTS[i] === __filename) {
+    arguments_trimmed = ARGUMENTS.slice(i + 1);
+    break;
+  }
+
+// Start Puppeteer testing
 (async () => {
+
 
   let promises = [];
   // Create Browser
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: HEADLESS_MODE});
 
   // Go to login page, login, and then close the page
-  const login_page = await browser.newPage();
-  console.log("Connecting to " + DOMAIN);
-  await login_page.goto(DOMAIN, {waitUntil: 'networkidle2'});
-  console.log("Page loaded. Executing actions in the tests folder.");
+  await do_test(tests.Login, browser, json_data)
+    .catch((() => {
+      process.exit(0);
+    }));
 
-  await action_classes.Login(login_page, USERNAME, PASSWORD)
-    .then((reason)=>{
-      console.log("Login:",reason);
-    })
-    .catch((reason => {
-      console.error("Login Error:", reason);
-    }))
-    .then(()=>{
-      login_page.close();
-    });
-
-
-
-  // Execute each test
-  for(let key in tests)
-  {
-    let page = await browser.newPage();
-    let singleTest = new tests[key](page, json_data);
-    singleTest.then((reason)=>{
-      console.error(`${test.toString()}: PASS`);
-      save_test_success(key, reason, true,test_results);
-    })
-      .catch((reason )=>{
-        console.error(`${test.toString()}: FAIL\n\t${reason}`);
-        save_test_success(key, reason, false, test_results);
-      });
-
-    // console.log("Executing test",test.toString());
-    await test.execute();
-
-  }
+  if (arguments_trimmed.length > 0)
+  // Execute the tests passed in as arguments
+    for (let key in arguments_trimmed) {
+      await do_test(tests[arguments_trimmed[key]], browser, json_data);
+    }
+  else
+    // Execute every test
+    for (let key in tests) {
+      if (key === "Login")
+        continue;
+      await do_test(tests[key], browser, json_data);
+    }
 
 
   await Promise.all(promises);
-  console.log("results:", test_results);
+
+  // Enabled for testing
+  console.log(test_results);
+
+  // Save log
+  fs.writeFileSync(RESULTS_FILE, JSON.stringify(test_results), 'utf8');
+
 
   // Exit from Node
   browser.close();
