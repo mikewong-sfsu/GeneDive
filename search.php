5a3bbc5b88ef56439c5db2764eb9371f615bf2b8
John Lazzarini <jlazzari@mail.sfsu.edu>
2018-05-21 23:29:20 -0700

    Adjusted color confidence score intervals

--- search.php
+++ search.php
@@ -42,7 +42,7 @@
         <h5>Minimum Confidence Score</h5>
         <span class="min-prob-slider-value">0.70</span>
       </div>
-      <input id="exp1" class="slider-selection min-prob-slider" type="text" name="min-prob-value" data-provider="slider" data-slider-min="0" data-slider-max="1" data-slider-step="0.01" data-slider-value="0.7" style="width:100%">
+      <input id="exp1" class="min-prob-slider" type="text" name="min-prob-value" data-provider="slider" data-slider-min="0" data-slider-max="1" data-slider-step="0.01" data-slider-value="0.7" style="width:100%">
       <div class="btn-group" style="width: 100%">
       <button data-toggle="tooltip-initial" data-placement="bottom" title="Apply filter for interactions with confidence score of at least 0.70.  WARNING: May increase query response time.  " class="low-button btn btn-default">Low</button>
       <button data-toggle="tooltip-initial" data-placement="bottom" title="Apply filter for interactions with confidence score of at least 0.85" class="medium-button btn btn-default">Medium</button>
@@ -165,7 +165,7 @@
 
     <!-- Graph View -->
     <div class="graph-view panel-bottom">
-                        <button type="button" class="module control-module btn-group btn btn-default reset-graph" data-toggle="tooltip-initial" data-placement="bottom" title="Regenerate the positions of the graph nodes" disabled> <span class="button-text">Redraw Graph</span></button>
+                        <button type="button" class="module control-module btn-group btn btn-default reset-graph" data-toggle="tooltip-initial" data-placement="bottom" title="Regenerate the positions of the graph nodes" disabled> <i class="fa fa-refresh" aria-hidden="true"></i> <span class="button-text">Redraw Graph</span></button>
       <div class="absent">
         <div>View Absent Nodes List</div>
       </div>
