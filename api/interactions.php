844d514d867560f8217a672bfe43472f9e4d13ad
Jack <jack-cole@users.noreply.github.com>
2018-08-06 11:18:31 -0700

    Fix for Content-Length being dropped in the header

--- api/interactions.php
+++ api/interactions.php
@@ -2,22 +2,23 @@
 
 include_once "../auth.php";
 
-  $pdo = new PDO( 'sqlite:../data/data.sqlite');
+$pdo = new PDO( 'sqlite:../data/data.sqlite');
 
-  $gid = $_GET['ids'];
-  $minProb = $_GET['minProb'];
-  $query = NULL;
+$gid = $_GET['ids'];
+$minProb = $_GET['minProb'];
 
-    $gids = explode(",",preg_replace('/[^0-9A-Za-z:,]/', "", $_GET['ids']));
+$query = NULL;
 
-    $prepared_slots = array_fill(0, sizeof($gids), "?");
-    $prepared_slots = implode(" , ", $prepared_slots);
+$gids = explode(",",preg_replace('/[^0-9A-Za-z:,]/', "", $_GET['ids']));
 
-  if ( sizeof($gids) > 1 ) {
-    $query = "SELECT * FROM interactions WHERE geneids1 IN ( $prepared_slots ) AND geneids2 IN ( $prepared_slots ) AND probability >= ? ORDER BY probability;";
-  } else {
-    $query = "SELECT * FROM interactions WHERE (geneids1 = $prepared_slots OR geneids2 = $prepared_slots) AND probability >= ? ORDER BY probability;";
-  }
+$prepared_slots = array_fill(0, sizeof($gids), "?");
+$prepared_slots = implode(" , ", $prepared_slots);
+
+if ( sizeof($gids) > 1 ) {
+  $query = "SELECT * FROM interactions WHERE geneids1 IN ( $prepared_slots ) AND geneids2 IN ( $prepared_slots ) AND probability >= ? ORDER BY probability;";
+} else {
+  $query = "SELECT * FROM interactions WHERE (geneids1 = $prepared_slots OR geneids2 = $prepared_slots) AND probability >= ? ORDER BY probability;";
+}
 
 $stmt = $pdo->prepare($query);
 
@@ -28,10 +29,23 @@ if(!$stmt)
 }
 else {
 
+  $stmt->execute(array_merge($gids, $gids, [$minProb]));
 
-    $stmt->execute(array_merge($gids, $gids, [$minProb]));
-//    echo $query;
-//    print_r(array_merge($gids, $gids, [$minProb]));
-    echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  // Save count of interactions
+  if(isset($_GET['queryKey']))
+  {
+    $queryKey = $_GET['queryKey'];
+    $queryVal = "interactions_$queryKey";
+    $_SESSION[ $queryVal] = sizeof($results);
+  }
+
+  $final_string = json_encode($results);
+  $size = mb_strlen($final_string, '8bit');
+
+  header('Content-Length: '.$size);
+  header('Content-Range: 0'); // Content header is dropped unless this is set to 0
+  echo $final_string;
 }
 ?>
