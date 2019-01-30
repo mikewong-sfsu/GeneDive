f7866357c4c5a9fb6507676908af352f2dcedafe
Mike Wong <mikewong@sfsu.edu>
2019-01-29 21:01:47 -0800

    Adding back new Docker install process after rolling back to fbebd42436cd4d9e4ab4325a8bbe94a542c68c4f

--- backend/docker/install/link.sh
+++ backend/docker/install/link.sh
@@ -1,11 +1,8 @@
-# Allow permissions so user database can be updated for registrations
-chmod a+w /usr/local/genedive/data /usr/local/genedive/data/users.sqlite
-
-# Link the current test data for immediate usage
-ln -s /usr/local/genedive/data/data.all.sqlite                     /usr/local/genedive/data/data.sqlite
-ln -s /usr/local/genedive/data/json.all/symbol_id.json             /var/www/html/static/genedive/json/symbol_id.json
-ln -s /usr/local/genedive/data/json.all/gene_id.json               /var/www/html/static/genedive/json/gene_id.json
-ln -s /usr/local/genedive/data/json.all/chemical_id.json           /var/www/html/static/genedive/json/chemical_id.json
-ln -s /usr/local/genedive/data/json.all/disease_id.json            /var/www/html/static/genedive/json/disease_id.json
-ln -s /usr/local/genedive/data/json.all/matrix_id.json             /var/www/html/static/genedive/json/matrix_id.json
-ln -s /usr/local/genedive/data/json.all/adjacency_matrix.json.zip  /var/www/html/static/genedive/json/adjacency_matrix.json
+## Link the current test data for immediate usage
+# if [ ! -L /usr/local/genedive/data/data.sqlite                         ]; then ln -s /usr/local/genedive/data/data.all.sqlite                     /usr/local/genedive/data/data.sqlite;                         fi
+# if [ ! -L /var/www/html/static/genedive/json/symbol_id.json            ]; then ln -s /usr/local/genedive/data/json.all/symbol_id.json             /var/www/html/static/genedive/json/symbol_id.json;            fi
+# if [ ! -L /var/www/html/static/genedive/json/gene_id.json              ]; then ln -s /usr/local/genedive/data/json.all/gene_id.json               /var/www/html/static/genedive/json/gene_id.json;              fi
+# if [ ! -L /var/www/html/static/genedive/json/chemical_id.json          ]; then ln -s /usr/local/genedive/data/json.all/chemical_id.json           /var/www/html/static/genedive/json/chemical_id.json;          fi
+# if [ ! -L /var/www/html/static/genedive/json/disease_id.json           ]; then ln -s /usr/local/genedive/data/json.all/disease_id.json            /var/www/html/static/genedive/json/disease_id.json;           fi
+# if [ ! -L /var/www/html/static/genedive/json/matrix_id.json            ]; then ln -s /usr/local/genedive/data/json.all/matrix_id.json             /var/www/html/static/genedive/json/matrix_id.json;            fi
+# if [ ! -L /var/www/html/static/genedive/json/adjacency_matrix.json.zip ]; then ln -s /usr/local/genedive/data/json.all/adjacency_matrix.json.zip  /var/www/html/static/genedive/json/adjacency_matrix.json.zip; fi
--- backend/docker/install/setup.sh
+++ backend/docker/install/setup.sh
@@ -4,13 +4,24 @@
 mkdir -p /var/www/html/static/genedive/json
 
 # Update OS and install SQLite3 and Python utilities
-# apt-get update
-# apt-get install -y apt-utils
-# apt-get install -y \
-# 	sqlite3 \
-# 	libsqlite3-dev \
-# 	python3-pip
+apt-get update
+apt-get install -y apt-utils
+apt-get install -y \
+        sqlite3 \
+        libsqlite3-dev \
+        python3-pip
+
+# Delete paths
+rm -rf /usr/local/genedive /var/www/html
+ln -s /genedive/backend /usr/local/genedive
+ln -s /genedive/frontend /var/www/html
+
+# Allow access to genedive path
+chmod a+rx /genedive
 
 # Create path for sessions
-mkdir -p /usr/local/genedive/sessions 
+mkdir -p /usr/local/genedive/sessions
 chmod a+w /usr/local/genedive/sessions
+
+# Allow permissions so user database can be updated for registrations
+chmod a+w /genedive/backend/data /genedive/backend/data/users.sqlite
