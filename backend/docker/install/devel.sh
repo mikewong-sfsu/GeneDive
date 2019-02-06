ede26b5905c10466335195c7102fd820bbec522e
Mike Wong <mikewong@sfsu.edu>
2019-02-06 01:47:44 -0800

    Adding NodeJS and Puppeteer to the Docker build

--- backend/docker/install/devel.sh
+++ backend/docker/install/devel.sh
@@ -4,8 +4,14 @@
 apt-get update
 apt-get install -y \
         apt-utils \
+		gnupg2 \
         git \
         vim
+
+# Get NodeJS v10 and Puppeteer
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
+apt-get install -y nodejs
+cd /usr/lib/node_modules && npm i puppeteer
 
 # Add keys
 mkdir -p /root/.ssh
@@ -13,11 +19,12 @@ chmod 700 /root/.ssh
 cp /genedive/backend/docker/genedive-github*.key /root/.ssh
 chmod 400 /root/.ssh/genedive-github*.key
 
-# Add SSH agent commands to .bashrc 
+# Add SSH agent commands and Node Modules path to .bashrc 
 cat << EOF >> /root/.bashrc
 eval \`ssh-agent -s\` > /dev/null 2>&1
 ssh-add -D 2>/dev/null
 ssh-add /root/.ssh/genedive-github.key 2>/dev/null
+export NODE_PATH=\`npm root -g\`
 EOF
 
 cat << EOF > /root/.ssh/config
