a7f0293b9ba2e8177a775765733272fef0a2f316
bishtvaishali <vaishali.bisht01@gmail.com>
2019-04-14 18:37:51 -0700

    added SortingColumn featur, modified and renamed DownloadResults_FeatureMixin.js

--- docs/gen/custom_modules/mixin/DownloadResultsFeature_Mixin.js
+++ docs/gen/custom_modules/mixin/DownloadResultsFeature_Mixin.js
@@ -2,58 +2,53 @@
 /**
  @class		DownloadResultsTest
  @brief		Test to check if Download Results, downloads the file and report errors.       
- @details   Path: the files are currently downloaded in the Tests folder only. 
+ @details   Path: Please provide the location/path of your download folder in the GeneDiveAPI_params_example.json. 
+            As by default it gets downloaded in the 'Downloads folder'.
  @authors	Vaishali Bisht vbisht1@mail.sfsu.edu
  @ingroup	tests
 */
 
-let Test = require('./Test');
 
-class DownloadResultsFeature_Mixin extends Test {
+class DownloadResultsFeature_Mixin{
 
-    validateHighlight(page){
-        const PAGE = this.page;
+    validateDownload(PAGE, downloadLocation) {
 
         const fs = require('fs');
-
         return new Promise(async (resolve, reject) => {
             try {
                 await PAGE.click('button.btn.btn-default.download');
                 await PAGE.waitFor(2000);
 
-                //  var dir = '/Users/vaishalibisht/Downloads/';      
                 await PAGE.click('button.ajs-button.ajs-ok');
-                await PAGE._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: './' });
+                await PAGE._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: downloadLocation });
 
                 //Download folder of the user
-                var dir = './';
-                console.log('dir', __dirname);
-                var files = fs.readdirSync(dir);
+                 console.log('DownloadLocation', downloadLocation);
+                var files = fs.readdirSync(downloadLocation);
                 const filteredFiles = files.filter((file) => { return file.indexOf('GeneDive-') !== -1; });
 
                 //sort the files according to time stamp
                 filteredFiles.sort(function (a, b) {
-                    return fs.statSync(dir + b).mtime.getTime() -
-                        fs.statSync(dir + a).mtime.getTime();
+                    return fs.statSync(downloadLocation + b).mtime.getTime() -
+                        fs.statSync(downloadLocation + a).mtime.getTime();
                 });
 
-                console.log({ filteredFiles });
+                // console.log({ filteredFiles });
 
                 const date = new Date();
                 const dateTimeStr = date.getFullYear() + (date.getMonth()).pad(2) + (date.getDate()).pad(2);
-                console.log('dateTIME sTR', date.getMonth());
+                console.log('dateTIME MONTH: ', date.getMonth());
 
                 const downloadedFile = filteredFiles[0];
                 if (downloadedFile.includes(`GeneDive-${dateTimeStr}`) && downloadedFile.endsWith('zip')) {
                     //delete the downloaded file
-                    fs.unlink((dir + downloadedFile), function (err) {
+                    fs.unlink((downloadLocation + downloadedFile), function (err) {
                         if (err) throw err;
                         console.log('Downloaded File deleted!');
                     });
-                    resolve(thisClass.createResponse(true, 'Test Passed', 0));
-
+                    resolve();
                 } else {
-                    reject('Test failed: File is not downloaded in your Downloads folder');
+                    reject('DownloadResultsFeature_Mixin: File is not downloaded in your selected folder');
                 }
             }
             catch (e) {
@@ -61,9 +56,8 @@ class DownloadResultsFeature_Mixin extends Test {
             }
 
         });
-
     }
-    
+
 }
 
 
