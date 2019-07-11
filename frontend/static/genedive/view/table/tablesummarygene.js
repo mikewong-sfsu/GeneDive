8bc047e603c3e09bf2f42afa883367ab4d13d2c3
Mike Wong <mikewong@sfsu.edu>
2019-07-11 04:08:04 +0000

    Continuing to improve results table styling

--- frontend/static/genedive/view/table/tablesummarygene.js
+++ frontend/static/genedive/view/table/tablesummarygene.js
@@ -35,11 +35,11 @@ class TableSummaryGene extends ResultsTable {
     tr.append( $(document.createElement("th")).text( "" ).css("width","4%" ) );
     tr.append( $(document.createElement("th")).html( "DGR<sub>1</sub>" ).css("width","10%").attr({ id : 'th-dgr1', "toggle": "tooltip", "title": "Disease, Gene, or Drug Entity related to your query"}) );
     tr.append( $(document.createElement("th")).html( "DGR<sub>2</sub>" ).css("width","10%").attr({ id : 'th-dgr2', "toggle": "tooltip", "title": "Disease, Gene, or Drug Entity related to your query"}) );
-    tr.append( $(document.createElement("th")).text( "# Mentions" ).css("width","4%").addClass("numeric rotate").attr({ id : 'th-mentions', "toggle": "tooltip", "title": "Number of interactions between other DGRs and your query"}) );
-    tr.append( $(document.createElement("th")).text( "# Articles" ).css("width","4%").addClass("numeric rotate").attr({ id : 'th-articles', "toggle": "tooltip", "title": "Number of articles that were accessed by the relationship algorithm"}) );
+    tr.append( $(document.createElement("th")).text( "# Mentions" ).css("width","60px").addClass("numeric rotate").attr({ id : 'th-mentions', "toggle": "tooltip", "title": "Number of interactions between other DGRs and your query"}) );
+    tr.append( $(document.createElement("th")).text( "# Articles" ).css("width","60px").addClass("numeric rotate").attr({ id : 'th-articles', "toggle": "tooltip", "title": "Number of articles that were accessed by the relationship algorithm"}) );
     tr.append( $(document.createElement("th")).text( "Conf. Score Dist." ).css("width","140px").attr({ id : 'th-cscore-dist', "toggle": "tooltip", "title": "Shows the confidence distribution between articles and suggested relationship confidence"}) );
-    tr.append( $(document.createElement("th")).html( "Max Conf.<br>Score" ).css("width","8%").addClass("numeric").attr({ id : 'th-cscore-max', "toggle": "tooltip", "title": "The closer this score is to one, the more likely it is for the corresponding relationship(s) to be accurate"}) );
-    tr.append( $(document.createElement("th")).text( "Sample Excerpt" ).css("min-width","40%").attr({ id : 'th-excerpt', "toggle": "tooltip", "title": "A selection from the article that the algorithm selected to imply a relationship"}) );
+    tr.append( $(document.createElement("th")).html( "Max Conf.<br>Score" ).css("width","80px").addClass("numeric").attr({ id : 'th-cscore-max', "toggle": "tooltip", "title": "The closer this score is to one, the more likely it is for the corresponding relationship(s) to be accurate"}) );
+    tr.append( $(document.createElement("th")).text( "Sample Excerpt" ).css("width","calc( 80% - 340px )").attr({ id : 'th-excerpt', "toggle": "tooltip", "title": "A selection from the article that the algorithm selected to imply a relationship"}) );
 
     this.table.append(thead);
   }
