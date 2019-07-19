40f1c07237e1ebf47af83f0930fadd267470afdf
Mike Wong <mikewong@sfsu.edu>
2019-07-19 14:40:16 -0700

    Alertify OK button now uses "success" theme

--- frontend/static/genedive/alertify-defaults.js
+++ frontend/static/genedive/alertify-defaults.js
@@ -1,4 +1,4 @@
 alertify.defaults.transition   = 'slide';
-alertify.defaults.theme.ok     = 'btn btn-primary';
+alertify.defaults.theme.ok     = 'btn btn-success';
 alertify.defaults.theme.cancel = 'btn btn-danger';
 alertify.defaults.theme.input  = 'form-control';
