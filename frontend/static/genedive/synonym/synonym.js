10a2f763d466ceaa9d6ff98a5c409c716dea5fe2
Mike Wong <mikewong@sfsu.edu>
2019-07-25 20:41:19 +0000

    Restoring synonym.js to previous functioning version. This is a mess.

--- frontend/static/genedive/synonym/synonym.js
+++ frontend/static/genedive/synonym/synonym.js
@@ -1,23 +1,29 @@
 class Synonym {
 
-  // ============================================================
-  static names( terms ) {
-  // ============================================================
-    return terms.reduce(( map, term ) => { map[ term.id ] = term.name; return map; }, {});
+  static buildIdSymbolMap ( search_set ) {
+    let map = {};
+
+    search_set
+    // TODO: Why was this filter here? Disabled
+      //.filter( set => set.type == "gene" )
+      .forEach( set => {
+      map[set.ids] = set.name;
+    });
+
+    return map;
   }
 
-  // ============================================================
-  static findSynonyms( terms, interactions ) {
-  // ============================================================
-    let names   = this.names( terms );
-    let symbols = terms.map( term => term.name );
+  static findSynonyms( search_set, interactions ) {
+    let id_symbol_map = this.buildIdSymbolMap( search_set );
+    let symbols = search_set.map( set => set.name );
 
     interactions.forEach ( i => {
-      i.mention1_synonym = symbols.includes( i.mention1 ) ? null : names[ i.geneids1 ] || null;
-      i.mention2_synonym = symbols.includes( i.mention2 ) ? null : names[ i.geneids2 ] || null;
+      i.mention1_synonym = symbols.includes(i.mention1) ? null : id_symbol_map[i.geneids1] || null;
+      i.mention2_synonym = symbols.includes(i.mention2) ? null : id_symbol_map[i.geneids2] || null;
     });
 
     return interactions;
   } 
+
 }
 
