36a4b1d30eaca1d047a7c04986d963159dfc7915
Mike Wong <mikewong@sfsu.edu>
2019-12-21 00:11:08 +0000

    Last few tweaks on menu UI

--- frontend/menu/data.php
+++ frontend/menu/data.php
@@ -1,16 +1,16 @@
 <div class="menu dropdown">
   <button class="btn btn-lg btn-link dropdown-toggle" type="button" id="menu-dropdown-button" data-toggle="dropdown"><span class="fa fa-bars"></span></button>
-  <ul class="dropdown-menu dropdown-menu-right">
+  <ul class="dropdown-menu dropdown-menu-right" style="margin-left: 20px; min-width: 240px;">
     <li class="dropdown-header">Data Sources</li>
-    <li class="dropdown-item"><a class="datasources" data-toggle="tooltip-initial" title="Select different data sources to query">Select Data Source</a></li>
-    <li class="dropdown-item"><a class="datasource-add" href="datasource/manage.php" target="_blank" data-toggle="tooltip-initial" title="Import or remove your own DGR interaction data.">Add/Remove a Data Source</a></li>
+    <li class="dropdown-item" style="cursor: pointer;"><a class="datasources" data-toggle="tooltip-initial" title="Select different data sources to query"><span class="fas fa-database"></span><span class="fas fa-check-square" style="font-size: 8pt; margin-left: 1px"></span> Select Data Source</a></li>
+    <li class="dropdown-item" style="cursor: pointer;"><a class="datasource-add" href="datasource/manage.php" target="_blank" data-toggle="tooltip-initial" title="Import or remove your own DGR interaction data."><span class="fas fa-database"></span><span class="fas fa-plus-square" style="font-size: 8pt; margin-left: 1px"></span> Add/Remove Data Source</a></li>
     <li class="divider" role="separator"></li>
     <li class="dropdown-header">Sessions Results</li>
-    <li class="dropdown-item"><a class="download-module upload" data-toggle="tooltip-initial" title="Upload a previously downloaded GeneDive Session zip file to resume that saved session.">Upload Session</a></li>
-    <li class="dropdown-item"><a class="download-module download" data-toggle="tooltip-initial" title="Download the current graph image, the current state, state history, and a CSV of the interactions as a zip file.">Download Session</a></li>
+    <li class="dropdown-item" style="cursor: pointer;"><a class="download-module upload" data-toggle="tooltip-initial" title="Upload a previously downloaded GeneDive Session zip file to resume that saved session."><span class="fas fa-upload"></span> Upload Session</a></li>
+    <li class="dropdown-item" style="cursor: pointer;"><a class="download-module download" data-toggle="tooltip-initial" title="Download the current graph image, the current state, state history, and a CSV of the interactions as a zip file."><span class="fas fa-download"></span> Download Session</a></li>
     <li class="divider" role="separator"></li>
     <li class="dropdown-header">More Information</li>
-    <li class="dropdown-item"><a class="help"  data-toggle="tooltip-initial" href="/help.html" target="_blank" title="Quickstart guide on how to use GeneDive and interpret results.">Help</a></li>
-    <li class="dropdown-item"><a class="about" data-toggle="tooltip-initial" href="/about.html" target="_blank" title="Citation, architecture and design, contact, and general information about GeneDive">About</a></li>
+    <li class="dropdown-item" style="cursor: pointer;"><a class="help"  data-toggle="tooltip-initial" href="/help.html" target="_blank" title="Quickstart guide on how to use GeneDive and interpret results."><span class="fas fa-question-circle"></span> Help</a></li>
+    <li class="dropdown-item" style="cursor: pointer;"><a class="about" data-toggle="tooltip-initial" href="/about.html" target="_blank" title="Citation, architecture and design, contact, and general information about GeneDive"><span class="fas fa-info-circle"></span> About</a></li>
   </ul>
 </div>
