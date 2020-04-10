08264a55b806724fb4facf21ad67737475426bb7
Mike Wong <mikewong@sfsu.edu>
2020-04-10 11:58:01 -0700

    Updating docker image build system. GeneDive Docker Image now weighs in at 1.5GB, ~25% of current size at 5.8GB.

--- backend/docker.compose/scripts/init.py
+++
@@ -1,9 +0,0 @@
-from lib import Database, ENVIRONMENT_DATA_FOLDER, ENVIRONMENT_NAME
-import os
-
-# Create the data folder
-if not os.path.exists(ENVIRONMENT_DATA_FOLDER):
-    os.makedirs(ENVIRONMENT_DATA_FOLDER)
-
-db = Database(ENVIRONMENT_NAME)
-db.build_schema()
