39980eee935ebbee16f6da253347c3a867811928
Mike Wong <mikewong@sfsu.edu>
2020-12-08 17:12:18 -0800

    Updating label for User-provided data import with GeneDive Docker

--- frontend/menu/data.php
+++ frontend/menu/data.php
@@ -1,6 +1,6 @@
 <div class="menu dropdown">
   <button class="btn btn-lg btn-link dropdown-toggle" type="button" id="menu-dropdown-button" data-toggle="dropdown"><span class="fa fa-bars"></span></button>
-  <ul class="dropdown-menu dropdown-menu-right" style="margin-left: 20px; min-width: 240px;">
+  <ul class="dropdown-menu dropdown-menu-right" style="margin-left: 20px; min-width: 200px;">
 <?php if( is_local_client()): ?>
     <li class="dropdown-header">Manage Data Sources</li>
     <li class="dropdown-item" style="cursor: pointer;"><a class="datasource-select" data-toggle="tooltip-initial" title="Select different data sources to query"><span class="fas fa-database"></span><span class="fas fa-check-square" style="font-size: 8pt; margin-left: 1px"></span> Select Data Source</a></li>
@@ -13,8 +13,8 @@
     <li class="dropdown-item" style="cursor: pointer;"><a class="download-module upload" data-toggle="tooltip-initial" title="Upload a previously downloaded GeneDive Session zip file to resume that session."><span class="fas fa-upload"></span> Restore Session</a></li>
     <li class="divider" role="separator"></li>
 <?php if( file_exists( 'static/genedive/docker/images/genedive-docker.gz' )): ?>
-    <li class="dropdown-header">GeneDive Docker Image (GDI)</li>
-    <li class="dropdown-item" style="cursor: pointer;"><a class="docker" data-toggle="tooltip-initial" href="download.php" title="Download the GeneDive Docker image. The GeneDive Docker image will allow you to import your own structured DGR data for search and visualization in GeneDive." target="_blank"><span class="fab fa-docker"></span> Download GeneDive</a></li>
+    <li class="dropdown-header">Importing User Data<br>&nbsp;with GeneDive Docker</li>
+    <li class="dropdown-item" style="cursor: pointer;"><a class="docker" data-toggle="tooltip-initial" href="download.php" title="Download the GeneDive Docker image. The GeneDive Docker image will allow you to import your own structured DGR data for search and visualization in GeneDive." target="_blank"><span class="fab fa-docker"></span> GeneDive Docker</a></li>
 <?php endif; ?>
     <li class="divider" role="separator"></li>
     <li class="dropdown-header">More Information</li>
